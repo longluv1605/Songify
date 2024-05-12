c728615cd6be635e4ece86a57ecd3bbe908dd9f2
phanhieeus <phanhieupkkq@gmail.com>
2024-05-12 11:23:22 +0700

    feat: add filter by label

--- backend/controllers/filter.controller.ts
+++ backend/controllers/filter.controller.ts
@@ -16,12 +16,23 @@ class FilterController {
         try {
             const userId = parseInt(req.userId as string);
             const genre = req.query.genre;
-            const sql: string = `SELECT m.id, m.title, m.cover_img_url, GROUP_CONCAT(DISTINCT mg.genre_name ORDER BY mg.genre_name SEPARATOR ', ') AS genres, ROUND(AVG(r.value), 1) AS average_rating
+            const label = req.query.label;
+            let movies;
+            if (genre !== undefined || null ) {
+                const sql: string = `SELECT m.id, m.title, m.cover_img_url, GROUP_CONCAT(DISTINCT mg.genre_name ORDER BY mg.genre_name SEPARATOR ', ') AS genres, ROUND(AVG(r.value), 1) AS average_rating
                                     FROM movie m
                                     LEFT JOIN movie_genre mg ON m.id = mg.movie_id
                                     LEFT JOIN user_rating r ON m.id = r.movie_id
-                                    GROUP BY m.id HAVING genres LIKE '%${genre}%' ORDER BY average_rating DESC`;
-            const movies = await this.db.query(sql, [genre]);
+                                    GROUP BY m.id HAVING genres ILIKE '%${genre}%' ORDER BY average_rating DESC`;
+                movies = await this.db.query(sql, [genre]);
+            }else if (label !== undefined || null ) {
+                const sql: string = `SELECT m.id, m.title, m.cover_img_url, GROUP_CONCAT(DISTINCT ml.label_name ORDER BY ml.label_name SEPARATOR ', ') AS labels, ROUND(AVG(r.value), 1) AS average_rating
+                                    FROM movie m
+                                    LEFT JOIN movie_label ml ON m.id = ml.movie_id
+                                    LEFT JOIN user_rating r ON m.id = r.movie_id
+                                    GROUP BY m.id HAVING labels ILIKE '%${label}%' ORDER BY average_rating DESC`;
+                movies = await this.db.query(sql, [label]);
+            }
 
             const results: {
                 movies: any,
