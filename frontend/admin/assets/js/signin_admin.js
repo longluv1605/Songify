0a125b2f2105322ed5a79d15ab5f140c0febea00
Trần Tiến Nam <117735708+NamSeko@users.noreply.github.com>
2024-05-16 21:12:00 +0700

    Thêm js cho login của admin

--- frontend/admin/assets/js/signin_admin.js
+++ frontend/admin/assets/js/signin_admin.js
@@ -1,24 +1,20 @@
-const postAccount = async() => {
+const postAccountAdmin = async (data) => {
     try {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
-        // console.log(email, password);
         if(email === '' || password === ''){
             alert('Please enter email and password');
             return;
         }
-        const response = await axios.post('http://localhost:8080/api/login', {
+        const response = await axios.post('http://localhost:8080/api/admin/login', {
             username: email,
             password: password
         });
-        // console.log(response);
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('movieid', "");
-        localStorage.setItem('genre', "");
-        localStorage.setItem('search', "");
-        window.location.href = 'http://localhost:3000/home';
+        localStorage.setItem('token_admin', response.data.token);
+
+        window.location.href = 'http://localhost:3000/admin/catalog';
     } catch (error) {
-        let deny_access = document.querySelector("#check-person label")
+        let deny_access = document.querySelector("#check-admin label");
         deny_access.style.display = "block";
     }
 };
@@ -27,6 +23,6 @@ const signin = document.getElementById('signin');
 
 document.addEventListener("DOMContentLoaded", function(){
     signin.addEventListener('click', function(){
-        postAccount();
+        postAccountAdmin();
     });
 });
