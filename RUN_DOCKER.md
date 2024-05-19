4d0ea5a870a740df3adaeef60c735af64e62ab22
Long <longtrong53@gmail.com>
2024-05-20 05:55:31 +0700

    add: Docker

---
+++ RUN_DOCKER.md
@@ -0,0 +1,17 @@
+# Welcome
+
+## Single container
+
+### Backend
+
+1. `docker build -t backend .`
+2. `docker run backend`
+
+### Frontend
+
+1. `docker build -t frontend .`
+2. `docker run frontend`
+
+## Docker-compose
+
+1. `docker-compose up`
--- backend/models/utils/recommender.utils.ts
+++ backend/models/utils/recommender.utils.ts
@@ -85,12 +85,54 @@ class Recommender {
             const movieId = input.movieId;
 
             if (movieId) {
-                const sql: string = `SELECT * FROM user_favorite WHERE user_id = ? AND movie_id = ?;`;
+                const sql: string = `
+                SELECT 
+                    m.id, 
+                    m.title, 
+                    m.cover_img_url, 
+                    mg.genres, 
+                    r.average_rating, 
+                    ml.label, 
+                    mv.views
+                FROM 
+                    (SELECT * FROM movie WHERE id IN (SELECT movie_id FROM user_favorite WHERE user_id = ? AND movie_id = ?)) m
+                LEFT JOIN 
+                    (SELECT movie_id, GROUP_CONCAT(DISTINCT genre_name ORDER BY genre_name SEPARATOR ', ') AS genres FROM movie_genre GROUP BY movie_id) mg ON m.id = mg.movie_id
+                LEFT JOIN 
+                    (SELECT movie_id, ROUND(AVG(value), 1) AS average_rating FROM user_rating GROUP BY movie_id) r ON m.id = r.movie_id
+                LEFT JOIN 
+                    (SELECT movie_id, label_name AS label FROM movie_label) ml ON m.id = ml.movie_id
+                LEFT JOIN 
+                    (SELECT movie_id, view AS views FROM movie_view) mv ON m.id = mv.movie_id
+                GROUP BY 
+                    m.id, m.title, m.cover_img_url, ml.label
+                `;
                 const favorite = await Database.query(sql, [userId, movieId]);
                 return favorite;
             }
 
-            const sql: string = `SELECT * FROM user_favorite WHERE user_id = ?;`;
+            const sql: string = `
+            SELECT 
+                m.id, 
+                m.title, 
+                m.cover_img_url, 
+                mg.genres, 
+                r.average_rating, 
+                ml.label, 
+                mv.views
+            FROM 
+                (SELECT * FROM movie WHERE id IN (SELECT movie_id FROM user_favorite WHERE user_id = ?)) m
+            LEFT JOIN 
+                (SELECT movie_id, GROUP_CONCAT(DISTINCT genre_name ORDER BY genre_name SEPARATOR ', ') AS genres FROM movie_genre GROUP BY movie_id) mg ON m.id = mg.movie_id
+            LEFT JOIN 
+                (SELECT movie_id, ROUND(AVG(value), 1) AS average_rating FROM user_rating GROUP BY movie_id) r ON m.id = r.movie_id
+            LEFT JOIN 
+                (SELECT movie_id, label_name AS label FROM movie_label) ml ON m.id = ml.movie_id
+            LEFT JOIN 
+                (SELECT movie_id, view AS views FROM movie_view) mv ON m.id = mv.movie_id
+            GROUP BY 
+                m.id, m.title, m.cover_img_url, ml.label
+            `;
             const favorite = await Database.query(sql, [userId]);
             return favorite;
         } catch (err) {
